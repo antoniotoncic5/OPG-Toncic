36fc3e0399f6e488b9952f58d683187ca1b4d1cf
Antonio Tončić <antoniotoncic5@gmail.com>
2026-03-17 19:25:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuToggle = document.getElementById("menu-toggle");
+const mainNav = document.getElementById("main-nav");
+
+if (menuToggle && mainNav) {
+    menuToggle.addEventListener("click", function () {
+        mainNav.classList.toggle("show");
+    });
+}
